5bd92eeffd61534389969389e9ef5dd0aea0d970
Ari Carr <acarr@brandeis.edu>
2018-02-02 03:45:45 -0500

    Changed Login to User

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -4,8 +4,8 @@ class AuthenticationsController < Devise::OmniauthCallbacksController
   # This method is called to enable users to log in with Shibboleth
   def saml
     hash = request.env['omniauth.auth']
-    new_user = Login.find_by(uid: hash['extra']['raw_info'].attributes['urn:oid:0.9.2342.19200300.100.1.1'][0]).nil?
-    @user = Login.from_saml(hash)
+    new_user = User.find_by(uid: hash['extra']['raw_info'].attributes['urn:oid:0.9.2342.19200300.100.1.1'][0]).nil?
+    @user = User.from_saml(hash)
     session[:user_id] = @user.id
     flash[:success] = "Welcome, #{@user.name}!"
     if new_user
