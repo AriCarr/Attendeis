ddffc8d4077755f5e71937e89dd4400de1c7bb20
Ari Carr <acarr@brandeis.edu>
2018-02-02 03:44:57 -0500

    Changed Login to User

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,7 +1,7 @@
 # This controller handles the callback after authenticating with Shibboleth
-class Logins::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def saml
-    @user = Login.from_omniauth(request.env['omniauth.auth'])
+    @user = User.from_omniauth(request.env['omniauth.auth'])
 
     if @user.persisted?
       sign_in_and_redirect @user, :event => :authentication
