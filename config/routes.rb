d814eef617749befa837e66776048701c6b3bc21
Ari Carr <acarr@brandeis.edu>
2018-02-05 11:53:04 -0500

    Fixed login bug

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,18 @@
 Rails.application.routes.draw do
+  get '/users/auth/:provider/callback', to: 'sessions#create'
+
   resources :courses
   resources :users
-
-  root to: 'pages#index'
-
   devise_for :users, :controllers => { :omniauth_callbacks => "authentications"}
-  devise_scope :users do
+
+  devise_scope :user do
     get 'sign_in', :to => 'devise/sessions#new', :as => :new_user_session
     get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
   end
 
   get '/login/:user_id' => 'sessions#login_local' if Rails.env.development?
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'pages#index'
+
 end
