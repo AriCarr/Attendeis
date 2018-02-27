b3ef32876d5a71d768d555bd4741c921b4818899
Ari Carr <acarr@brandeis.edu>
2018-02-26 22:16:22 -0500

    rails_admin fix

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -2,8 +2,6 @@ class Attendance < ApplicationRecord
   belongs_to :course
   has_many :checkins
   has_many :students, class_name: 'User', through: :checkins
-  has_many :teachings
-  has_many :teachers, class_name: 'User', through: :teachings
   validates :password, uniqueness: true, allow_nil: true
 
   def timestamp
