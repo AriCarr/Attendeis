122122f484da98c78fb02070bf4b2a1dfc4e6fe1
Ari Carr <acarr@brandeis.edu>
2018-03-09 00:28:12 -0500

    Added ability to override students' status

--- app/helpers/attendances_helper.rb
+++ app/helpers/attendances_helper.rb
@@ -1,7 +1,7 @@
 module AttendancesHelper
 
-  def absent_without_nil
-    @absent.map { |u| User.new(uid: u.uid, name: (u.name.nil? ? '' : u.name)) }
+  def without_nil(list)
+    list.map { |u| User.new(uid: u.uid, name: (u.name.nil? ? '' : u.name)) }
   end
 
 end
